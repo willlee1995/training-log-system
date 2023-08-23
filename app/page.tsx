91419dd9c9209418ec9fc43f17506bb9a9abe870
Will Lee <willlee19951009@gmail.com>
2023-08-23 17:22:36 +0800

    second

--- app/page.tsx
+++ app/page.tsx
@@ -49,73 +49,32 @@ export default function MusicPage() {
                   <h2 className="text-2xl font-semibold tracking-tight">
                     My progress summary
                   </h2>
-                  <Tabs defaultValue="music" className="h-full space-y-6">
+                  <Tabs defaultValue="progress" className="h-full space-y-6">
                     <div className="space-between flex items-center">
                       <TabsList>
-                        <TabsTrigger value="music" className="relative">
+                        <TabsTrigger value="progress" className="relative">
                           My progress
                         </TabsTrigger>
-                        <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
-                        <TabsTrigger value="live" disabled>
-                          All cases
-                        </TabsTrigger>
+                        <TabsTrigger value="cases">All cases</TabsTrigger>
                       </TabsList>
                       <div className="ml-auto mr-4">
                         <Button>
                           <PlusCircledIcon className="mr-2 h-4 w-4" />
-                          Add music
+                          Add procedure
                         </Button>
                       </div>
                     </div>
                     <TabsContent
-                      value="music"
+                      value="progress"
                       className="border-none p-0 outline-none"
                     >
-                      <Separator className="my-4" />
-                      <div className="relative"></div>
-                      <div className="mt-6 space-y-1">
-                        <h2 className="text-2xl font-semibold tracking-tight">
-                          Made for You
-                        </h2>
-                        <p className="text-sm text-muted-foreground">
-                          Your personal playlists. Updated daily.
-                        </p>
-                      </div>
-                      <Separator className="my-4" />
-                      <div className="relative">
-                        <ScrollArea>
-                          <div className="flex space-x-4 pb-4">
-                            {madeForYouAlbums.map((album) => (
-                              <AlbumArtwork
-                                key={album.name}
-                                album={album}
-                                className="w-[150px]"
-                                aspectRatio="square"
-                                width={150}
-                                height={150}
-                              />
-                            ))}
-                          </div>
-                          <ScrollBar orientation="horizontal" />
-                        </ScrollArea>
-                      </div>
+                      Table for the progress
                     </TabsContent>
                     <TabsContent
                       value="podcasts"
                       className="h-full flex-col border-none p-0 data-[state=active]:flex"
                     >
-                      <div className="flex items-center justify-between">
-                        <div className="space-y-1">
-                          <h2 className="text-2xl font-semibold tracking-tight">
-                            New Episodes
-                          </h2>
-                          <p className="text-sm text-muted-foreground">
-                            Your favorite podcasts. Updated daily.
-                          </p>
-                        </div>
-                      </div>
-                      <Separator className="my-4" />
-                      <PodcastEmptyPlaceholder />
+                      Table for the All Cases
                     </TabsContent>
                   </Tabs>
                 </div>
